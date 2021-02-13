0c5219d7669d32bcff2c124dc7f3cf81550aaf22
ayoub_jouini <ayoubjouini135@gmail.com>
2021-02-13 22:07:23 +0100

    part-1

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ function menuCreat(i) {
     var element = document.createElement("a");
     var node = document.createTextNode(nav_menu[i]);
     element.appendChild(node);
-    var htmlElement = document.getElementById("menu");
+    var htmlElement = document.getElementsByClassName("menu")[0];
     htmlElement.appendChild(element);
 }
 for (i = 0; i < 5; i++) {
     menuCreat(i);
 }
+const navSlide = () => {
+    const menuBtn = document.querySelector('.menu-btn');
+    const nav = document.querySelector('.menu');
+    menuBtn.addEventListener('click', () => {
+        nav.classList.toggle('menu-active');
+    });
+}
+navSlide();
