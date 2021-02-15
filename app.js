165c6833940ee277c670d17f5ac5c3735ef5a5a0
ayoub_jouini <ayoubjouini135@gmail.com>
2021-02-15 21:58:34 +0100

    part-3,part-4,part-5

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// nav-bar
 const nav_menu = ["Home", "New", "Clothes", "Shoes", "Accessories"];
 function menuCreat(i) {
     var element = document.createElement("a");
@@ -17,6 +18,9 @@ const navSlide = () => {
     });
 }
 navSlide();
+
+// part-1
+
 const imgHover = (j) => {
     const imgDiv = document.querySelectorAll('.txt');
     const part1Img = document.querySelectorAll('.part1_img');
@@ -30,6 +34,9 @@ const imgHover = (j) => {
 for (j = 0; j < 4; j++) {
     imgHover(j);
 }
+
+// part-2
+
 let clothes = [
     {
         "id": 1,
@@ -131,7 +138,7 @@ function jeantArt() {
     div2.setAttribute("class", "artc-txt");
     artc.appendChild(div1);
     artc.appendChild(div2);
-    var htmlArtc = document.getElementById("jeans-div");
+    var htmlArtc = document.getElementById("prod-div");
     var butn = document.getElementsByClassName("more")[0];
     htmlArtc.appendChild(artc)
 }
@@ -156,7 +163,7 @@ function artcLast(v) {
     var btn = document.createElement("button");
     color.setAttribute("class", "color");
     siz.setAttribute("class", "siz");
-    var btnNode = document.createTextNode("Buy");
+    var btnNode = document.createTextNode("More details");
     var htmlColSiz = document.getElementsByClassName("col-siz")[v];
     var htmlBtn = document.getElementsByClassName("artc-btn")[v];
     htmlColSiz.appendChild(siz);
@@ -166,13 +173,15 @@ function artcLast(v) {
 }
 function range(nt, k) {
     var siz = document.getElementsByClassName("siz")[nt];
+    var color = document.getElementsByClassName("color")[nt];
     var price = document.getElementsByClassName("price")[nt];
     var artcImg = document.getElementsByClassName("artc-img")[k];
-    //var color = document.getElementsByClassName("color")[k];
     var name1 = document.getElementsByClassName("name")[nt];
     var nameH = document.createElement("h2");
     var artcImgI = document.createElement("img");
     var nameNode = document.createTextNode(clothes[k].name);
+    var colorNode = document.createTextNode(clothes[k].color);
+    color.appendChild(colorNode);
     nameH.appendChild(nameNode);
     name1.appendChild(nameH);
     var sizNode = document.createTextNode(clothes[k].size);
@@ -182,9 +191,6 @@ function range(nt, k) {
     artcImgI.setAttribute("class", "img-part2");
     artcImgI.setAttribute("src", clothes[k].image);
     artcImg.appendChild(artcImgI);
-
-    //ll color mezel
-
 }
 var nt = 0;
 for (k = 0; k < 10; k++) {
